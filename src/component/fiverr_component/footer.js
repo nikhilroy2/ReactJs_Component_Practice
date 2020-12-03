41b669b90c23142bd003466138652b74bb44ad35
Nikhil <nikhilchandraroypoet@gmail.com>
2020-12-03 11:13:27 +0600

    first commit with fiverr footer practice

---
+++ src/component/fiverr_component/footer.js
@@ -0,0 +1,111 @@
+import React, { Fragment } from 'react';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+function Fiver_Footer() {
+    return (
+        <Fragment>
+            <footer>
+                <div className="container-xl">
+                    <div className="row row-cols-lg-5 row-cols-md-3 row-cols-1 custom_row_list">
+                        <div className="col h-auto p-0">
+                            <div className="wrapper m-1 p-3 shadow-sm rounded-lg h-100">
+                                <div className="wrapper_head">
+                                    <h6>Categories</h6>
+                                </div>
+                                <div className="wrapper_body">
+                                    <ul className="list-group">
+                                        <div className="list-group-item "><a href="#" className="list-group-link">Graphics & Design</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Digital Marketing</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Writing & Translation</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Video & Animation</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Music & Audio</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Programming & Tech</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Business</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Lifestyle</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Sitemap</a></div>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col h-auto p-0">
+                            <div className="wrapper m-1 p-3 shadow-sm rounded-lg h-100">
+                                <div className="wrapper_head">
+                                    <h6>About</h6>
+                                </div>
+                                <div className="wrapper_body">
+                                    <ul className="list-group">
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Careers</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Press & News</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Partnerships</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Privacy Policy</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Terms of Service</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Intellectual Property Claims</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Investor Relations</a></div>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col h-auto p-0">
+                            <div className="wrapper m-1 p-3 shadow-sm rounded-lg h-100">
+                                <div className="wrapper_head">
+                                    <h6>Support</h6>
+                                </div>
+                                <div className="wrapper_body">
+                                    <ul className="list-group">
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Help & Support</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Trust & Safety!</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Selling on Fiverr</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Buying on Fiverr</a></div>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col h-auto p-0">
+                            <div className="wrapper m-1 p-3 shadow-sm rounded-lg h-100">
+                                <div className="wrapper_head">
+                                    <h6>Community</h6>
+                                </div>
+                                <div className="wrapper_body">
+                                    <ul className="list-group">
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Events</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Blog</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Forum</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Community Standards</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Podcast</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Affiliates</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Invite a Friend</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Become a Seller</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Fiverr Elevate <span>Exclusive Benefits</span></a></div>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col h-auto p-0">
+                            <div className="wrapper m-1 p-3 shadow-sm rounded-lg h-100">
+                                <div className="wrapper_head">
+                                    <h6>More From Fiverr</h6>
+                                </div>
+                                <div className="wrapper_body">
+                                    <ul className="list-group">
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Fiverr Business</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Fiverr Pro</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Fiverr Studios</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Fiverr Logo Maker</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Fiverr Guides</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">Get Inspired</a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">ClearVoice <span>Content Marketing</span> </a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link">AND CO <span>Invoice Software</span> </a></div>
+                                        <div className="list-group-item"><a href="#" className="list-group-link"> Learn<span>Online Courses</span> </a></div>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
+        </Fragment>
+    )
+}
+
+export default Fiver_Footer
