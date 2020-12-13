7431673234184678b5d38afbe47ac36489a3d4b3
FENR1 USER <fenr1user@biadsconsulting.org>
2020-12-13 13:15:58 +0600

    component title added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Fiver_Footer from './component/fiverr_component/footer';
 function App() {
   return (
     <div className="App">
+      <h1 className="text-center">Fiverr Component</h1>
       <Fiver_Footer/>
      
     </div>
